ddbbfc4e300cbff935990b42015b15e253a2c0eb
Md Saiful Islam <saifulhere4u@gmail.com>
2024-02-19 21:16:43 +0600

    Shopify Auth Controller updated

--- app/Http/Controllers/Admin/ShopifyAuthController.php
+++ app/Http/Controllers/Admin/ShopifyAuthController.php
@@ -9,6 +9,21 @@ class ShopifyAuthController extends Controller
 {
     public function redirectToShopify (Request $request)
     {
+
+        $handle = $request->input('handle');
+        $appkey = $request->input('appkey');
+        $callbackUrl = route('shopify.callback');
+        $authorizationUrl = 'https://'.$handle.'myshopline.com/admin/oauth-web' .
+        '?appkey=' . $appkey .'?responseType=code'.
+        '&scope=' . 'read_products' . // The requested scopes
+        '&redirect_uri=' . url('https://www.eventbookingbd.com/shopify/callback');
+
+        // Redirect users to the Shopify OAuth authorization URL
+        return redirect($authorizationUrl);
+    }
+
+    public function handleShopifyCallback (Request $request)
+    {
         dd($request);
     }
 }
