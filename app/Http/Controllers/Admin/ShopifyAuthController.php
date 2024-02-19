7f0444b2c3ef8c73250573c7db82b53998f9f8a1
Md Saiful Islam <saifulhere4u@gmail.com>
2024-02-19 21:31:52 +0600

    shopify auth controller

--- app/Http/Controllers/Admin/ShopifyAuthController.php
+++ app/Http/Controllers/Admin/ShopifyAuthController.php
@@ -12,14 +12,15 @@ class ShopifyAuthController extends Controller
 
         $handle = $request->input('handle');
         $appkey = $request->input('appkey');
-        $callbackUrl = route('shopify.callback');
-        $authorizationUrl = 'https://'.$handle.'.myshopline.com/admin/oauth-web' .
-        '?appKey=' . $appkey .'?responseType=code'.
-        '&scope=' . 'read_products' . // The requested scopes
-        '&redirectUri=' . url('https://www.eventbookingbd.com/shopify/callback');
-
+        $callbackUrl = route('shopify.callback'); // Assuming this is correct
+        $authorizationUrl = 'https://' . $handle . '.myshopline.com/admin/oauth-web' .
+            '?appKey=' . $appkey . '&responseType=code' .
+            '&scope=' . 'read_products' . // The requested scopes
+            '&redirectUri=' . urlencode($callbackUrl);
+        
         // Redirect users to the Shopify OAuth authorization URL
         return redirect($authorizationUrl);
+        
     }
 
     public function handleShopifyCallback (Request $request)
